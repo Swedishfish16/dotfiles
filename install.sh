5d3ba9a7cd40c7fd76ce3fb893c3552f0d8fb403
Swedishfish16 <kevryan16@gmail.com>
2024-05-20 13:17:07 +1000

    Added install script

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+#Define array of configurations and their corresponding symbolic link targets
+configurations=(
+	"~/dotfiles/.zshrc: ~/.zshrc"
+	"~/dotfiles/.tmux: ~/.tmux"
+	"~/dotfiles/.tmux.conf: ~/.tmux.conf"
+	"~/dotfiles/.bashrc: ~/.bashrc"
+	"~/dotfiles/.config: ~/.config"
+	"~/dotfiles/.vimrc: ~/.vimrc"
+) 
+
+#Functon to create symbolic links
+create_links() {
+	for config in "${configurations[@]}"; do
+		#split config and link target using:
+		IFS=':' read -ra parts <<< "$config"
+		config_file="${parts[0]}"
+		config_target="${parts[1]}"
+
+		#Create symlink
+		ln -s "$config_file" "$link_target"
+
+		#Print message
+		echo "Created symbolic link for $config_file at $link_target"
+	done
+}
+
+#Function to install Zsh
+install_zsh() {
+	sudo apt install zsh
+}
+
+#Function to install Tmux
+install_tmux(){
+	sudo apt install tmux
+}
+
+#Fucntion to install Neovim
+install_neovim() {
+	#Download neovim AppImage
+	wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -P /tmp
+
+	#Make the AppImage executeable
+	chmod +x /tmp/nvim.appimage
+
+	#Move the AppImage to a directory in Path
+	sudo mv /tmp/nvim.appimage /usr/local/bin/nvim
+}
+
+#Main function
+main() {
+	echo "Installing ZSH..."
+	install_zsh
+
+	echo "Installing Tmux..."
+	install_tmux
+	
+	echo "Installing Neovim..."
+	install_neovim
+
+	echo "Creating symbolic links for configuration..."
+	create_links
+
+	echo "Setup complete."
+}
+
+#Run main function
+main
+
